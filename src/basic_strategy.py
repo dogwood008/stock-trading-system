ebe9a272792347b1772a50ec780ced57f6db7041
dogwood008 <dogwood008+github@gmail.com>
2022-03-21 23:23:18 +0900

    :sparkles: Add a basic strategy

--- src/basic_strategy.py
+++ src/basic_strategy.py
@@ -3,12 +3,15 @@ from backtrader import Order
 
 import pytz
 
-from logging import getLogger, StreamHandler, Formatter, DEBUG, INFO, WARN
+from logging import getLogger, StreamHandler, FileHandler, \
+     Formatter, DEBUG, INFO, WARN
 
 class BasicStrategy(bt.Strategy):
     params = (
-        ('size', 1000),
+        ('tick_counter', 0),
+        ('size', 100),
         ('smaperiod', 5),
+        ('log_name', 'basic_strategy.log'),
     )
     def _log(self, txt, loglevel=INFO, dt=None):
         ''' Logging function for this strategy '''
@@ -16,7 +19,7 @@ class BasicStrategy(bt.Strategy):
         # 内部的に linebuffer.datetime() を呼び出す
         # https://github.com/mementum/backtrader/blob/e2674b1690f6366e08646d8cfd44af7bb71b3970/backtrader/linebuffer.py#L386-L388
         dt = dt or self.datas[0].datetime.datetime(
-            ago=0, tz=pytz.timezone("Asia/Tokyo"))
+            ago=0, tz=pytz.timezone('Asia/Tokyo'))
         self._logger.log(loglevel, '%s, %s' % (dt.isoformat(), txt))
 
     def _debug(self, txt, dt=None):
@@ -27,39 +30,79 @@ class BasicStrategy(bt.Strategy):
 
     def __init__(self, loglevel=DEBUG):
         # Keep a reference to the "close" line in the data[0] dataseries
+        formatter = Formatter('[%(levelname)s] %(message)s')
         self._dataclose = self.datas[0].close
         self._logger = getLogger(__name__)
         self.handler = StreamHandler()
         self.handler.setLevel(loglevel)
+        self.handler.setFormatter(formatter)
+        self.fhandler = FileHandler(self.p.log_name, encoding='utf-8')
+        self.fhandler.setLevel(loglevel)
+        self.fhandler.setFormatter(formatter)
         self._logger.setLevel(DEBUG)
         self._logger.addHandler(self.handler)
+        self._logger.addHandler(self.fhandler)
         self._logger.propagate = False
-        self.handler.setFormatter(
-                Formatter('[%(levelname)s] %(message)s'))
         self.sma = bt.indicators.SimpleMovingAverage(
             self.datas[0], period=self.params.smaperiod)
 
 
     def notify_order(self, order: Order) -> None:
-        ''' backtraderから呼ばれる '''
-        if order.status == Order.Completed:
-            if order.isbuy():
-                self._info('BUY: %.2f' % order.executed.price)
+        '''
+        backtraderから呼ばれる
+        - Submitted: sent to the broker and awaiting confirmation
+        - Accepted: accepted by the broker
+        - Partial: partially executed
+        - Completed: fully exexcuted
+        - Canceled/Cancelled: canceled by the user
+        - Expired: expired
+        - Margin: not enough cash to execute the order.
+        - Rejected: Rejected by the broker
+        '''
+        # https://www.backtrader.com/docu/order/#:~:text=of%20an%20order-,Order%20Status%20values,-The%20following%20are
+        if order.status == Order.Accepted:
+            self._info('Accepted: [%s] %.2f * %d' % 
+                (self._buy_sell_in_str(order),
+                 order.price or 0, order.size or 0))
+        elif order.status == Order.Completed:
+            # 注文が通った
+            executed = order.executed
+            self._info('Completed [%s]: %.2f * %d' % 
+                (self._buy_sell_in_str(order),
+                 executed.price, executed.size))
+        elif order.status in \
+            (Order.Canceled, Order.Expired, Order.Margin, Order.Rejected):
+            # 注文が通らなかった
+            import pdb; pdb.set_trace()
 
     def next(self):
+        self.p.tick_counter += 1
         # Simply log the closing price of the series from the reference
-        self._debug('[Close] = %.2f' % (self._dataclose[0]))
+        # 今カーソルがある日時のtickにおける売買成立値
+        self._debug('[%6d] [Close] = %.2f' %
+            (self.p.tick_counter, self._dataclose[0]))
 
-        if self._dataclose[0] < self._dataclose[-1]:
-            # current close less than previous close
+        # もし5本連続で下がっているなら
+        if self.p.tick_counter >= 5 and \
+           self._is_falling_over_5_ticks():
+            # 1ティック前の値で売りオーダー（当日限り有効）
+            price = self._dataclose[-1]
+            size = self.p.size
+            self._info('Order: [sell] %.2f * %d' % (price, size))
+            self.sell(size=size, price=price, valid=Order.DAY)
 
-            if self._dataclose[-1] < self._dataclose[-2]:
-                # previous close less than the previous close
+    def _is_falling_over_5_ticks(self):
+        '''もし5本連続で下がっているならTrue'''
+        return self._dataclose[0] < self._dataclose[-1] < \
+           self._dataclose[-2] < self._dataclose[-3] < self._dataclose[-4]
 
-                # BUY, BUY, BUY!!! (with all possible default parameters)
-                self._info('BUY CREATE, %.2f' % self._dataclose[0])
-                self.buy(size=self.params.size)
+    def _buy_sell_in_str(self, order: Order) -> str:
+        if order.isbuy():
+            return 'buy'
+        elif order.issell():
+            return 'sell'
+        else:
+            raise 'Unknown type'
 
-            if self._dataclose[-2] * .97 > self._dataclose[-1]:
-                # 前日が前々日の3%を超える下落時に手仕舞い
-                self.close()
+    def stop(self):
+        self.fhandler.close()
--- src/main.py
+++ src/main.py
@@ -5,7 +5,6 @@ from datetime import datetime  # For datetime objects
 import os.path  # To manage paths
 import sys  # To find out the script name (in argv[0])
 
-import pytz
 
 # Import the backtrader platform
 import backtrader as bt
